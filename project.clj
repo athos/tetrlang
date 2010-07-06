92b11fe16e6e9bdcff4c3347605412e6ba835891
OHTA Shogo <athos0220@gmail.com>
2010-07-06 22:25:03 +0900

    modified project.clj for specifying main namespace

--- project.clj
+++ project.clj
@@ -1,1 +1,5 @@
-(defproject tetrlang "1.0.0-SNAPSHOT" :description "FIXME: write" :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"] [org.clojure/clojure-contrib "1.0-SNAPSHOT"]])
+(defproject tetrlang "1.0.0-SNAPSHOT"
+  :description "tetrlang interpreter"
+  :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
+		 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
+  :main tetrlang)
